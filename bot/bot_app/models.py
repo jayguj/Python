16a6649f1a0043a44004b22bcad5b6a60b1caf61
Jay Gujarathi <jaygujarathi1016@gmail.com>
2019-01-17 01:07:38 +0530

    Bot folder was added and committed successfully in local repo

---
+++ bot/bot_app/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+# Create your models here.
+class UserDetails(models.Model):
+    user_id=models.IntegerField(primary_key=True)
+    first_name=models.CharField(max_length=200)
+    last_name=models.CharField(max_length=200)
+    email=models.EmailField()
+    password=models.CharField(max_length=15)
+#
+class Requests(models.Model):
+    user_id=models.ForeignKey(UserDetails,on_delete=False)
+    user_message=models.CharField(max_length=500)
+
+class User_Responses(models.Model):
+    message=models.CharField(max_length=500)
+    opt1=models.CharField(max_length=200)
+    opt2=models.CharField(max_length=200)
+    opt3=models.CharField(max_length=200)
+    opt4=models.CharField(max_length=200)
